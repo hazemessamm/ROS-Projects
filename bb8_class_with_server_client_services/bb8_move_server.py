d5b0fe8886ac51c870797c4fca0196b8c5e70eac
Hazem Essam <hazemessamm@users.noreply.github.com>
2019-11-23 05:24:05 +0200

    Update bb8_move_server.py

--- bb8_class_with_server_client_services/bb8_move_server.py
+++ bb8_class_with_server_client_services/bb8_move_server.py
@@ -17,3 +17,7 @@ rospy.init_node('service_move_bb8_in_circle_server')
 my_service = rospy.Service('/move_bb8_in_circle', MyCustomServiceMessage , my_callback)
 rospy.loginfo("Service /move_bb8_in_circle Ready")
 rospy.spin()
+
+
+
+#you can run this by opening it in the first terminal using python bb8_move_server.py and in the second terminal write rosservice call /move_bb8_in_circle [TAB]+[TAB]
